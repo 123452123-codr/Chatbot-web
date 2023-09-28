03a32135d006e0fd9108e80f87a46466ccd5d062
123452123-codr <81916900+123452123-codr@users.noreply.github.com>
2023-09-28 16:25:24 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,10 @@
+var messages = document.getElementById("message");
+var text = document.getElementById("text");
+var button = document.getElementById("send");
+
+button.addEventListener("click", function(){
+  var newMessage = document.createElement("li");
+  newMessage.innerHTML = text.value;
+  messages.appendChild(newMessage);
+  text.value = "";
+});
